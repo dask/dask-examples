186d52bad319573d1be9fe9d2ded2490a92510f8
Bryan Weber <bryan.w.weber@gmail.com>
2022-05-04 14:15:44 -0500

    Fix Sphinx warnings from unlisted files

--- conf.py
+++ conf.py
@@ -50,7 +50,7 @@ nbsphinx_prolog = """
 
 .. admonition:: Live Notebook
 
-    You can run this notebook in a `live session <https://mybinder.org/v2/gh/dask/dask-examples/main?urlpath=lab/tree/{{ docname }}>`_ |Binder| or view it `on Github <https://github.com/dask/dask-examples/blob/main/{{ docname }}>`_.
+    You can run this notebook in a `live session <https://mybinder.org/v2/gh/dask/dask-examples/main?urlpath=lab/tree/{{ docname }}>`__ |Binder| or view it `on Github <https://github.com/dask/dask-examples/blob/main/{{ docname }}>`__.
 
 .. |Binder| image:: https://static.mybinder.org/badge_logo.svg
    :target: https://mybinder.org/v2/gh/dask/dask-examples/main?urlpath=lab/tree/{{ docname }}
@@ -117,7 +117,7 @@ html_theme = 'dask_sphinx_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
--- index.rst
+++ index.rst
@@ -23,6 +23,7 @@ You can run these examples in a live session here: |Binder|
    futures
    machine-learning
    xarray
+   resilience
 
 .. toctree::
    :maxdepth: 1
@@ -30,6 +31,8 @@ You can run these examples in a live session here: |Binder|
 
    dataframes/01-data-access
    dataframes/02-groupby
+   dataframes/03-from-pandas-to-dask
+   dataframes/04-reading-messy-data-into-dataframes
 
 .. toctree::
    :maxdepth: 1
@@ -70,7 +73,6 @@ You can run these examples in a live session here: |Binder|
    surveys/2021.ipynb
    surveys/2020.ipynb
    surveys/2019.ipynb
-   
 
 .. raw:: html
 
@@ -85,4 +87,3 @@ You can run these examples in a live session here: |Binder|
    ga('send', 'pageview');
    </script>
    <!-- End Google Analytics -->
-
